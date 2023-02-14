26565a553d8f29f396cb84bf34225e343a880693
Gabriel Pajot <github@gab.les-cactus.co>
2023-02-14 12:14:50 +0100

    feat!: use local-tuya BREAKING CHANGES: - `Device`, `FanSpeed`, `Mode` have been prefixed with `AC` - `Values` has been replaced by `ACState` - `ACDevice` is now an async context manager that needs to be entered properly before using - `ACDevice` methods have changed and are now async - `ACDevice.values` has been removed. A callback can be passed to the device init to receive state updates - Domoticz plugin name and key has changed, see migration notes.
    The goal is to bring additional features:
    - persistent and robust communication to the device
    - automatic remote device state updates (remotes can still be used)
    - configurable buffering for subsequent updates
    - Domoticz plugin using a dedicated thread
    
    Migrate Domoticz plugin:
    1. deactivate hardwares using `Airton AC`
    2. stop Domoticz
    3. python -m pip install --upgrade airton-ac
    4. python -m airton_ac.domoticz.install -p /path/to/domoticz/folder
    5. start Domoticz
    6. for each hardware change hardware type to `Tuya Airton AC`, and check config
    7. remove `plugins/AirtonAC` in Domoticz folder
    8. restart Domoticz

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,119 +1,85 @@
+import sys
+from typing import Dict, Union
+
 import pytest
 
-from airton_ac.device import Command, Device, FanSpeed, Mode, Values
+from airton_ac.device import ACDataPoint, ACDevice, ACFanSpeed, ACMode, ACState
 
 
-class TestDevice:
+def test_state_load():
+    values: Dict[str, Union[str, int, bool]] = {
+        ACDataPoint.POWER.value: True,
+        ACDataPoint.SET_POINT.value: 200,
+        ACDataPoint.TEMP.value: 215,
+        ACDataPoint.MODE.value: ACMode.HEAT.value,
+        ACDataPoint.FAN.value: ACFanSpeed.L1.value,
+        ACDataPoint.ECO.value: False,
+        ACDataPoint.LIGHT.value: True,
+        ACDataPoint.SWING.value: "un_down",
+        ACDataPoint.SWING_DIRECTION.value: ACDataPoint.SWING.value,
+        ACDataPoint.SLEEP.value: False,
+        ACDataPoint.HEALTH.value: True,
+    }
+    assert ACState.load(values) == ACState(
+        power=True,
+        set_point=20,
+        temp=21.5,
+        mode=ACMode.HEAT,
+        fan_speed=ACFanSpeed.L1,
+        eco=False,
+        light=True,
+        swing=True,
+        sleep=False,
+        health=True,
+    )
+
+
+class TestACDevice:
     @pytest.fixture()
     def device(self, mocker):
-        dev = Device(
-            _id="id",
-            address="address",
-            local_key="local_key",
-            set_point_offset=-1,
-            temp_offset=-2,
-        )
-        mocker.patch.object(
-            dev,
-            "_raw_values",
-            return_value={
-                Command.POWER.value: True,
-                Command.SET_POINT.value: 200,
-                Command.TEMP.value: 215,
-                Command.MODE.value: Mode.HEAT.value,
-                Command.FAN.value: FanSpeed.L1.value,
-                Command.ECO.value: False,
-                Command.LIGHT.value: True,
-                Command.SWING.value: "un_down",
-                Command.SWING_DIRECTION.value: Command.SWING.value,
-                Command.SLEEP.value: False,
-                Command.HEALTH.value: True,
-            },
-        )
-        return dev
+        mocker.patch("local_tuya.Device.__init__")
+        return ACDevice(mocker.Mock())
 
-    def test_values(self, device):
-        assert device.values() == Values(
-            power=True,
-            set_point=19,
-            temp=19.5,
-            mode=Mode.HEAT,
-            fan_speed=FanSpeed.L1,
-            eco=False,
-            light=True,
-            swing=True,
-            sleep=False,
-            health=True,
-        )
-
-    @pytest.mark.parametrize(
-        ("current", "update", "expected"),
-        [
-            # Nothing should do nothing.
-            ({}, {}, {}),
-            # Unchanged values should be ignored.
-            (
-                {Command.POWER.value: True, Command.HEALTH.value: False},
-                {Command.POWER.value: True, Command.HEALTH.value: True},
-                {Command.HEALTH.value: True},
-            ),
-            # Set point cannot be set on auto.
-            (
-                {
-                    Command.MODE.value: Mode.AUTO.value,
-                    Command.ECO.value: False,
-                    Command.SET_POINT.value: 170,
-                },
-                {Command.SET_POINT.value: 160},
-                {},
-            ),
-            # Set point cannot be set on eco.
-            (
-                {
-                    Command.MODE.value: Mode.HEAT.value,
-                    Command.ECO.value: True,
-                    Command.SET_POINT.value: 170,
-                },
-                {Command.SET_POINT.value: 160},
-                {},
-            ),
-            # Set point can be set for other modes.
-            (
-                {
-                    Command.MODE.value: Mode.HEAT.value,
-                    Command.ECO.value: False,
-                    Command.SET_POINT.value: 170,
-                },
-                {Command.SET_POINT.value: 160},
-                {Command.SET_POINT.value: 160},
-            ),
-        ],
+    @pytest.mark.skipif(
+        sys.version_info < (3, 8),
+        reason="requires python3.8 or higher for AsyncMock",
     )
-    def test__validate_payload(self, current, update, expected, device):
-        assert device._validate_payload(current, update) == expected
-
     @pytest.mark.parametrize(
         ("temp", "expected"),
         [
-            (19, 200),
-            (19.4, 200),
-            (19.7, 210),
+            (19, 190),
+            (19.4, 190),
+            (19.7, 200),
         ],
     )
-    def test_set_temperature(self, temp, expected, device, mocker):
-        mocker.patch.object(device, "_update", side_effect=lambda d: d)
-        assert device.set_temperature(temp) == {Command.SET_POINT.value: expected}
+    async def test_set_point(self, temp, expected, device, mocker):
+        update = mocker.patch.object(device, "_update")
 
+        await device.set_point(temp)
+
+        update.assert_awaited_once_with({ACDataPoint.SET_POINT: expected})
+
+    @pytest.mark.skipif(
+        sys.version_info < (3, 8),
+        reason="requires python3.8 or higher for AsyncMock",
+    )
     @pytest.mark.parametrize(
         ("swing", "expected_swing", "expected_direction"),
         [
-            (True, "un_down", Command.SWING.value),
+            (True, "un_down", ACDataPoint.SWING),
             (False, "off", "off"),
         ],
     )
-    def test_set_swing(self, swing, expected_swing, expected_direction, device, mocker):
-        mocker.patch.object(device, "_update", side_effect=lambda d: d)
-        assert device.set_swing(swing) == {
-            Command.SWING.value: expected_swing,
-            Command.SWING_DIRECTION.value: expected_direction,
-        }
+    async def test_switch_swing(
+        self, swing, expected_swing, expected_direction, device, mocker
+    ):
+        update = mocker.patch.object(device, "_update")
+
+        await device.switch_swing(swing)
+
+        update.assert_awaited_once_with(
+            {
+                ACDataPoint.SWING: expected_swing,
+                ACDataPoint.SWING_DIRECTION: expected_direction,
+            }
+        )
